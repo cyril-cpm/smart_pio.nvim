00d7e4f22e2d566b6185515b65eda871ab2530b5
CPM <cpm.cyril@gmail.com>
2025-11-09 07:54:37 +0100

    just remove some diagnostic because clangd is impossible

--- SelectEnv.sh
+++ SelectEnv.sh
@@ -34,21 +34,48 @@ then
 
 	idedata=$(platformio run -t idedata -e ${1} | jq --raw-input '. | fromjson?')
 	toolchainPath=$(dirname $(dirname $(echo $idedata | jq '.cxx_path' | grep -Po '[^"]*')))
-	target="riscv32-esp-elf"
-	sysroot="$toolchainPath/$target"
+	echo $toolchainPath
+	target="xtensa-esp32-elf"
+	sysroot="-isysroot=/home/cpm/.platformio/packages/toolchain-xtensa-esp-elf/xtensa-esp-elf"
+	echo $sysroot
+	toolchain=$(echo $idedata | jq --arg sysroot $sysroot '
+								[$sysroot]
+								+ ["-isystem" + .includes.toolchain.[]]
+								+ ["-I" + .includes.build.[]]
+								')
+	includes=$(echo $idedata | jq '["-I" + .includes.build.[]]')
+	includes=$(echo $includes)
 	echo -e "
 Diagnostics:
-	Suppress: [\"-Wvla-cxx-extension\"]
+	Suppress: [
+		\"-Wvla-cxx-extension\",
+		\"member_function_call_bad_type\",
+		\"unknown_typename\",
+		\"redefinition_different_typedef\",
+		\"pp_file_not_found\",
+		\"init_conversion_failed\",
+		\"typecheck_convert_incompatible_pointer\"
+		]
 	UnusedIncludes: None
 
 CompileFlags:
-	Add:
-		$(echo $idedata | jq '["-isystem" + .includes.toolchain.[]] + ["-xc++"]')
+	CompilationDatabase: \"${root}/.pio/build/${1}\"
+	Add: $toolchain
 	Remove: [\"-fno-shrink-wrap\", \"-fno-tree-switch-conversion\", \"-fstrict-volatile-bitfields\", \"-mlongcalls\"]
 " > "${root}/.clangd"
 
 if [[ ! -z $PLATFORMIO_INSTALL_ROOT ]]; then
-	ln -f "${root}/.clangd" "${PLATFORMIO_INSTALL_ROOT}/.clangd"
+	
+	echo -e "
+Diagnostics:
+	Suppress: [\"*\"]
+	UnusedIncludes: None
+
+CompileFlags:
+	CompilationDatabase: \"${root}/.pio/build/${1}\"
+	Add: $toolchain
+	Remove: [\"-fno-shrink-wrap\", \"-fno-tree-switch-conversion\", \"-fstrict-volatile-bitfields\", \"-mlongcalls\"]
+" > "${PLATFORMIO_INSTALL_ROOT}/.clangd"
 fi
 
 else
